bcbe03d2044a2705e72d54fec5506bc5ae6e62b1
Friederich Nietzsche <frnitzsche@yahoo.ca>
2025-07-13 20:14:15 +0300

    update

--- customization-script.sh
+++ customization-script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 host="wp-anishev.mywire.org"
+wpbackup="mywebsite.WordPress.2025-07-13.xml"
 sudo yum install docker -y && \
 sudo systemctl enable docker.service && \
 sudo systemctl start docker.service && \
@@ -15,6 +16,8 @@ sudo docker exec docker-compose-wordpress-wordpress-1 chmod +x wp-cli.phar && \
 sudo docker exec docker-compose-wordpress-wordpress-1 mv wp-cli.phar /usr/local/bin/wp && \
 # sudo wp core install --title='My Website' --url='wp-anishev.mywire.org'  --admin_user='alex' --admin_password='478312zxc' --admin_email='my@email.com'  --ssh=docker:docker-compose-wordpress-wordpress-1 --allow-root
 sudo docker exec docker-compose-wordpress-wordpress-1 wp core install --title='My Website' --url='wp-anishev.mywire.org'  --admin_user='alex' --admin_password='478312zxc' --admin_email='my@email.com' --allow-root && \
+sudo docker cp $wpbackup docker-compose-wordpress-wordpress-1:/ && \
+sudo docker exec docker-compose-wordpress-wordpress-1 wp import $wpbackup --authors=create
 
 sudo yum install dotnet-runtime-8.0 -y && \
 sudo wget --trust-server-names https://www.dynu.com/support/downloadfile/70 && \
