4d7e9650b94069257cb0ca71112e8c4f85cd7ec1
Friederich Nietzsche <frnitzsche@yahoo.ca>
2025-07-13 18:09:56 +0300

    working state

--- customization-script.sh
+++ customization-script.sh
@@ -26,28 +26,6 @@ sed '/root         \/usr\/share\/nginx\/html;/r'<(cat <<EOF
         }
 EOF
 ) -i -- /etc/nginx/nginx.conf && \
+
 sudo systemctl restart nginx.service && \
 sudo certbot --nginx -d $host -m my@mail.com --agree-tos -n
-
-        # location / {
-        #     proxy_pass http://127.0.0.1:8080;
-        #     proxy_set_header Host $host;
-        #     proxy_set_header X-Real-IP $remote_addr;
-        #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        #     proxy_set_header X-Forwarded-Proto $scheme;
-        # }
-
-
-
-
-
-# sed '/root         \/usr\/share\/nginx\/html;/r'<(cat <<EOF
-#         location / {
-#             proxy_pass http://127.0.0.1:8080;
-#             proxy_set_header Host \$host;
-#             proxy_set_header X-Real-IP \$remote_addr;
-#             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-#             proxy_set_header X-Forwarded-Proto \$scheme;
-#         }
-# EOF
-# ) -i -- /etc/nginx/nginx.conf
