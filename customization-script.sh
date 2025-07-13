26090d3f3c39535bec636336159051933e229a42
Friederich Nietzsche <frnitzsche@yahoo.ca>
2025-07-13 18:01:20 +0300

    update

--- customization-script.sh
+++ customization-script.sh
@@ -15,12 +15,22 @@ sudo systemctl restart dynu-ip-update-client.service && \
 sleep 5s && \
 sudo yum install nginx certbot certbot-nginx -y && \
 sudo sed -i "s/server_name  _;/server_name ${host};/g" /etc/nginx/nginx.conf && \
-sudo certbot --nginx -d $host -m my@mail.com --agree-tos -n
 
+sed '/root         \/usr\/share\/nginx\/html;/r'<(cat <<EOF
+        location / {
+            proxy_pass http://127.0.0.1:8080;
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
+        }
+EOF
+) -i -- /etc/nginx/nginx.conf
+
+sudo certbot --nginx -d $host -m my@mail.com --agree-tos -n
 
         # location / {
         #     proxy_pass http://127.0.0.1:8080;
-        #     # proxy_set_header Host ${host};
         #     proxy_set_header Host $host;
         #     proxy_set_header X-Real-IP $remote_addr;
         #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -31,3 +41,13 @@ sudo certbot --nginx -d $host -m my@mail.com --agree-tos -n
 
 
 
+# sed '/root         \/usr\/share\/nginx\/html;/r'<(cat <<EOF
+#         location / {
+#             proxy_pass http://127.0.0.1:8080;
+#             proxy_set_header Host \$host;
+#             proxy_set_header X-Real-IP \$remote_addr;
+#             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+#             proxy_set_header X-Forwarded-Proto \$scheme;
+#         }
+# EOF
+# ) -i -- /etc/nginx/nginx.conf
